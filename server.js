447b6959911c6f291e9879679e08afc1047722f8
ikdcheeseburger-arch <ikdcheeseburger@gmail.com>
2026-03-21 10:14:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -56,6 +56,7 @@ app.post("/chat", async (req, res) => {
     });
 
     const data = await response.json();
+console.log(data);
     const reply = data?.choices?.[0]?.message?.content || "No response";
 
     chatHistory.push({ role: "assistant", content: reply });
