c273c08636cb0e85d304b6cad19f5adcddbeee0a
ikdcheeseburger-arch <ikdcheeseburger@gmail.com>
2026-03-21 12:32:25 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-console.log("API KEY:", process.env.OPENROUTER_KEY);
 
+console.log("KEY LENGTH:", process.env.OPENROUTER_KEY?.length);
 const app = express();
 
 // ✅ Safe CORS
